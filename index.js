bfbcd16e4097edc7190a1c564b17e9abc9f27aa1
Glitch (probot-hello-world) <none>
2019-09-12 07:43:47 +0000

    🌉🚡 Checkpoint ./index.js:4790460/33

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = (app) => {
     // `context` extracts information from the event, which can be passed to
     // GitHub API calls. This will return:
     //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
-    const params = context.issue({body: 'Hello World!'})
+    const params = context.issue({body: 'Thank you for your '})
 
     // Post a comment on the issue
     return context.github.issues.createComment(params)
