919d96a9948e8a66b54ed183061ec4afc2013f39
Glitch (probot-hello-world) <none>
2019-09-13 08:43:12 +0000

    📼🏭 Checkpoint ./README.md:4790460/321 ./index.js:4790460/290

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 Recap Time Probot App for GitHub
 ======
 
-To get started using
+To get started using the bot, here are the steps to deploy this bot to your repo.
+
+- First, login to your GitHub account.
+- Then, [click here](https://github.com/apps/recap-time-probot-app/installations/new) to open
+installation page.
+- Select where the app to deploy.
+- After that, the app is successfully installed.
--- index.js
+++ index.js
@@ -7,7 +7,8 @@ module.exports = (app) => {
     // `context` extracts information from the event, which can be passed to
     // GitHub API calls. This will return:
     //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
-    const params = context.issue({body: 'Thank you for your '})
+    const params = context.issue({body: 'Thank you for installing Recap Time Probot App!' + '\n' + '\n' +
+                                 'To configure the app, see [https://supportcentral-madebythepins.freshdesk.com/solutions]'})
 
     // Post a comment on the issue
     return context.github.issues.createComment(params)
